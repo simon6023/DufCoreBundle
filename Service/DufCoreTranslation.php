a148f8c9868c65625de9c38fb6da2a524f0d1a08
simonduflosuzik <simon.duflos@uzik.com>
2016-10-20 18:14:59 +0200

    translations in controllers

--- Service/DufCoreTranslation.php
+++ Service/DufCoreTranslation.php
@@ -46,4 +46,47 @@ class DufCoreTranslation
 
         return $default_locale;
     }
+
+    public function findEntityTranslatable($params)
+    {
+        if (!isset($params['entity_name']))
+            return null;
+
+        $routing_service    = $this->container->get('duf_admin.dufadminrouting');
+        $locale             = (isset($params['locale'])) ? $params['locale'] : $this->container->getParameter('locale');
+        $entity_class       = (strpos($params['entity_name'], ':') !== false) ? $routing_service->getEntityClass($params['entity_name']) : $params['entity_name'];
+
+        if (substr($entity_class, 0, 1) == '\\')
+            $entity_class = substr($entity_class, 1, strlen($entity_class));
+
+        // get translations for entity
+        $translations   = $this->em->getRepository('Gedmo\Translatable\Entity\Translation')->findBy(
+                array(
+                    'objectClass'      => $entity_class,
+                    'field'            => $params['search_field'],
+                    'content'          => $params['search_value'],
+                )
+            );
+
+        if (!empty($translations)) {
+            $entity_tools               = $this->container->get('duf_core.dufcoreentitytools');
+            $translated_entity_id       = current($translations)->getForeignKey();
+            $translated_entity          = $this->em->getRepository($entity_class)->findOneById($translated_entity_id);
+
+            if (!empty($translated_entity)) {
+                foreach ($translations as $translation) {
+                    // get entity setter
+                    $setter         = $entity_tools->getEntitySetter($translated_entity, $translation->getField());
+
+                    if (null !== $setter) {
+                        $translated_entity->{$setter} = $translation->getContent();
+                    }
+                }
+
+                return $translated_entity;
+            }
+        }
+
+        return null;
+    }
 }
